689ea1d28fbbacdd8cb6a9437b10a5cfdee9b93a
Benjamin Meyers <meyersbs@users.noreply.github.com>
2017-03-28 10:47:00 -0400

    Update download.py

--- corpora/download.py
+++ corpora/download.py
@@ -1,5 +1,21 @@
-import requests
+import urllib
+import gzip
+import os
+import traceback
+
+downloader = urllib.URLopener()
 
 def download():
-
+    print("Downloading preparsed datasets..."
+    wikipedia = downloader.retrieve("http://people.rc.rit.edu/~bsm9339/corpora/stanford_politeness/wikipedia.parsed.json.gz", "wikipedia.parsed.json.gz")
+    stack_exchange = downloader.retrieve("http://people.rc.rit.edu/~bsm9339/corpora/stanford_politeness/stack-exchange.parsed.json.gz", "stack-exchange.parsed.json.gz")
+    print("Finished!")
+          
 def extract():
+    try:
+        print("Extracting preparsed datasets. This will take a while...")
+        open("wikipedia.parsed.json", "w").write(gzip.open("wikipedia.parsed.json.gz", "rb"))
+        open("stack-exchange.parsed.json", "w").write(gzip.open("stack-exchange.parsed.json.gz", "rb"))
+        print("Finished!")
+    except Exception as e:
+        pass
