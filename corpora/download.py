1e0cc7dcc666b5a9a9d92c7d2d8d737cd0a7dc7d
Benjamin Meyers <bsm9339@rit.edu>
2017-03-28 17:06:05 -0400

    Helper functions work as expected.

--- corpora/download.py
+++ corpora/download.py
@@ -1,26 +1,36 @@
-import urllib
 import gzip
 import sys
-import traceback
 
-downloader = urllib.URLopener()
+from urllib.request import urlretrieve
 
 def download():
-    print("Downloading preparsed datasets..."
-    wikipedia = downloader.retrieve("http://people.rc.rit.edu/~bsm9339/corpora/stanford_politeness/wikipedia.parsed.json.gz", "wikipedia.parsed.json.gz")
-    stack_exchange = downloader.retrieve("http://people.rc.rit.edu/~bsm9339/corpora/stanford_politeness/stack-exchange.parsed.json.gz", "stack-exchange.parsed.json.gz")
+    print("Downloading preparsed datasets...")
+    urlretrieve("http://people.rc.rit.edu/~bsm9339/corpora/stanford_politeness/wikipedia.parsed.json.gz", "wikipedia.parsed.json.gz")
+    urlretrieve("http://people.rc.rit.edu/~bsm9339/corpora/stanford_politeness/stack-exchange.parsed.json.gz", "stack-exchange.parsed.json.gz")
     print("Finished!")
-          
+
 def extract():
     try:
-        print("Extracting preparsed datasets. This will take a while...")
-        open("wikipedia.parsed.json", "w+").write(gzip.open("wikipedia.parsed.json.gz", "rb"))
-        open("stack-exchange.parsed.json", "w+").write(gzip.open("stack-exchange.parsed.json.gz", "rb"))
-        print("Finished!")
-    except Exception as e:
-        traceback.print_exception(extype, exvalue, extrace)
+        print("Extracting preparsed Wikipedia dataset. This will take a while...")
+        with gzip.open("wikipedia.parsed.json.gz", "rb") as infile:
+            with open("wikipedia.parsed.json", "wb+") as outfile:
+                for line in infile:
+                    outfile.write(line.decode("utf-8"))
 
-if __name__ = "__main__":
+        print("Extracting preparsed Stack Exchange dataset. This will take a while...")
+        with gzip.open("stack-exchange.parsed.json.gz", "rb") as infile:
+            with open("stack-exchange.parsed.json", "wb+") as outfile:
+                for line in infile:
+                    outfile.write(line.decode("utf-8"))
+
+        print("Finished!")
+    except FileNotFoundError as e:
+        print("Oops! It looks like you are missing some files. Try running "
+              "'python download.py download' to grab them!")
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) < 1:
         download()
